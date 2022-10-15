4ed763a21b509d9daff533b3d4013eacf3a712f5
Rafael <rafaelzica86@hotmail.com>
2022-10-14 21:42:41 -0300

    Criação da tela de descrição e organização da resposividade

--- src/pages/Descricao/index.jsx
+++ src/pages/Descricao/index.jsx
@@ -1,7 +1,45 @@
 import { useLocation } from "react-router-dom"
+import {SiGithub,SiSitepoint} from 'react-icons/si'
+import {
+  SiHtml5,
+  SiReact,
+  SiJavascript,
+  SiPhp,
+  SiCss3,
+  SiSass,
+  SiTailwindcss,
+
+ } from 'react-icons/si';
 import './Descricao_modules.css';
 
 const Descricao = () =>{
+  function getIcon(icon){
+    switch(icon){
+      case "HTML":
+       return <SiHtml5 />
+        break;
+      case "React" :
+        return <SiReact/>
+        break;
+      case "Javascript":
+        return <SiJavascript />
+        break;
+      case "PHP":
+        <SiPhp />
+        break;
+      case "CSS":
+        return <SiCss3 />
+        break;
+      case "SCSS":
+        return <SiSass />
+        break;
+      case "TailWind":
+        return <SiTailwindcss />
+        break;
+        default:
+          return <SiCss3 />
+  }
+  }
   const data=useLocation();
   const Infos=data.state
   
@@ -13,14 +51,17 @@ const Descricao = () =>{
          <h3>{Infos.Descricao}</h3>
       </div>
       <div className="linguagem">
-        <h2>Linguagem utilizada</h2>
-         <h3>{Infos.Linguagem}</h3>
+        <h2>Linguagens</h2>
+         <h3>Linguagem de Front-end: {getIcon(Infos.Linguagem)} {Infos.Linguagem}</h3>
+         <h3>Linguagem de Estilo: {getIcon(Infos.Estilo)} {Infos.Estilo == null ? "CSS" : Infos.Estilo}</h3>
       </div>
-      <div className="estilo">
-        <h2>Linguagem de estilo utilizada</h2>
-        <h3>{Infos.Estilo}</h3>
-      </div>
+    <div className="buttons">
+      <h2>Conheça mais </h2>
+      <a href={Infos.Link} target="_blank" rel="noreferrer">< SiSitepoint/>Site</a>
+      <a href={Infos.Git} target="_blank" rel="noreferrer"><SiGithub style={{color: "red"}} />GitHub</a>
 
+    
+    </div>
     </div>
   )
 }
